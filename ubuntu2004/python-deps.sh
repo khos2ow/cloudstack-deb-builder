dfd561b755b5e9b5211e5fc1543c9090f7f4e594
Boris V.Kuznetsov <socnetfpga@gmail.com>
2021-08-11 21:53:48 +0300

    Started to impl a multi stage build
    Signed-off-by: Boris V.Kuznetsov <socnetfpga@gmail.com>

--- ubuntu2004/python-deps.sh
+++ ubuntu2004/python-deps.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/bash
-apt install -y wget libmysqlclient-dev python2-minimal
 
 # Setup Python2.7 pip
-wget https://bootstrap.pypa.io/pip/2.7/get-pip.py -O /tmp/get-pip.py
+wget --no-check-certificate https://bootstrap.pypa.io/pip/2.7/get-pip.py -O /tmp/get-pip.py
 python2 /tmp/get-pip.py
 
-pip install MySQL-python
-pip install mysql-connector-python
+pip2 install MySQL-python
+pip2 install mysql-connector-python
 
 # Alternative Mysql Connector Setup for Python 2.7 
 # Taken from: https://stackoverflow.com/questions/63457213/how-to-install-python-mysqldb-for-python-2-7-in-ubuntu-20-04-focal-fossa
