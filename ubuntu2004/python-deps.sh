4c5a951ec7a33901b3c864f4e8c2252917483b73
Boris V.Kuznetsov <socnetfpga@gmail.com>
2021-10-18 19:41:05 +0300

    * Bump deps versions * Add pip2 upgrade

--- ubuntu2004/python-deps.sh
+++ ubuntu2004/python-deps.sh
@@ -9,5 +9,6 @@ ln -s $(which python2) /usr/bin/python
 # Taken from: https://stackoverflow.com/questions/63457213/how-to-install-python-mysqldb-for-python-2-7-in-ubuntu-20-04-focal-fossa
 wget -q --no-check-certificate https://raw.githubusercontent.com/paulfitz/mysql-connector-c/master/include/my_config.h -O /usr/include/mysql/my_config.h
 
+pip2 install --upgrade pip
 pip2 install MySQL-python
 pip2 install mysql-connector-python
